7430793ded7d255098ddbb67eb6360c0923ec095
EvilArtillery <melkijdruk@gmail.com>
2024-09-18 18:02:08 +0300

    speed_to_no_speed changed to energy calculations instead of differentials

--- Differential/Petrov_DS.c
+++ Differential/Petrov_DS.c
@@ -27,27 +27,29 @@ double oscillator_no_speed(double phi0, double dt, double l, double g){
 }
 
 double speed_to_no_speed(double phi0, double psi0, double dt, double l, double g){
-    double phit = phi0;
-    double psit = psi0;
-    double w = g/l;
+    double energy, height, angle, speed;
 
-    double phit1, psit1;
+    speed = psi0 * l;
 
-    while(psit*psi0 > 0){
-        phit1 = phit + (psit * dt);
-        psit1 = psit - (w * sin(phit) * dt); /*
-        printf("Angle: %lf\n", phit1);
-        printf("Speed: %lf\n\n", psit1); */
+    height = l * (1 - cos(phi0));
+    //e = (v^2)/2 + gh
+    energy = speed * speed / 2;
+    energy += g * height;
 
-        if(fabs(phit1) - M_PI > 1e-14){
-            return -1;
-        }
+    //e = gh = gl(1 - cos(phi))
+    height = energy / g;
+    height /= l;
 
-        psit = psit1;
-        phit = phit1;
+    height -= 1;
+    height *= -1;
+
+    if(fabs(height) > 1){
+        printf("\x1b[31mToo much energy!\x1b[0m\n");
+        return 0;
     }
-
-    return(oscillator_no_speed(phit, dt, l, g) - dt);
+    angle = acos(height);
+    
+    return(oscillator_no_speed(angle, dt, l, g) - dt);
 }
 
 int main(){
@@ -104,6 +106,7 @@ int main(){
     if(0 == psi0) result = oscillator_no_speed(phi0, dt, l, g);
     else result = speed_to_no_speed(phi0, psi0, dt, l, g);
 
+    if(0 == result) return 0;
     printf("%lf\n", result);
     return(1);
 }
