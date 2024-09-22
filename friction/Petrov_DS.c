b2742d2822454584ba93d423ac169bb17381a3ad
EvilArtillery <melkijdruk@gmail.com>
2024-09-22 11:16:44 +0300

    friction started

--- friction/Petrov_DS.c
+++ friction/Petrov_DS.c
@@ -2,3 +2,32 @@
 #include <stdlib.h>
 #include <math.h>
 
+int main(){
+    double speed0 = 0, angle0 = 0;
+
+    printf("Enter the initial speed: ");
+    if(1 != scanf("%lf", &speed0)){
+        perror("Scanf failed");
+        return -1;
+    }
+
+    if(speed0 <= 1e-14){
+        printf("\x1b[31mInitial speed has to be more than 0!\x1b[0m\n");
+        return 0;
+    }
+
+    printf("Enter the throw angle: ");
+    if(1 != scanf("%lf", &angle0)){
+        perror("Scanf failed");
+        return -1;
+    }
+
+    if(angle0 <= 1e-14){
+        printf("\x1b[31mThrow angle has to be more than 0!\x1b[0m\n");
+        return 0;
+    }
+    if(angle0 > M_PI_2){
+        printf("\x1b[31mThrow angle has to be less than pi/2!\x1b[0m\n");
+        return 0;
+    }
+}
