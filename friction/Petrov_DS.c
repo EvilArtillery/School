1bb78bfb5d9c059753c40a5dcfe5aff966236f35
EvilArtillery <melkijdruk@gmail.com>
2024-09-22 13:31:34 +0300

    friction work

--- friction/Petrov_DS.c
+++ friction/Petrov_DS.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 #include <math.h>
 
+int friction_instance(double* x, double* y, double* speed_x, double* speed_y, double friction){
+    
+}
+
 int main(){
-    double speed0 = 0, angle0 = 0;
+    double speed0 = 0, angle0 = 0, friction = 0;
 
     printf("Enter the initial speed: ");
     if(1 != scanf("%lf", &speed0)){
@@ -29,5 +33,28 @@ int main(){
     if(angle0 > M_PI_2){
         printf("\x1b[31mThrow angle has to be less than pi/2!\x1b[0m\n");
         return 0;
+    }
+
+    printf("Enter your friction coefficient: ");
+    if(1 != scanf("%lf", &friction)){
+        perror("Scanf failed");
+        return -1;
+    }
+
+    if(-1e-14 >= friction){
+        printf("\x1b[31mFriction can't be less than 0!\x1b[0m\n");
+        return 0;
+    }
+
+    double speedx = 0, speedy = 0, x = 0, y = 0;
+
+    speedx = speed0 * cos(angle0);
+    speedy = speed0 * sin(angle0);
+
+    int n = 0;
+
+    while(x > -1e-14){
+        friction_instance(&x, &y, &speedx, &speedy, friction);
+        n += 1;
     }
 }
