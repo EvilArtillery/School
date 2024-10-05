a20c5e81acc3d76df48a77e4c6332f02e3129f0c
EvilArtillery <melkijdruk@gmail.com>
2024-10-05 09:50:35 +0300

    Pendulum_BS added

---
+++ Differential/Petrov_DS_Pendulum_BS.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+double oscillator_no_speed(double phi0, double dt, double l, double g){
+    double phit = fabs(phi0);
+    if(1e-14 > phit) return 0;
+    double psit = 0;
+    double w = -g/l;
+    unsigned long long n = 0;
+
+    double psit1, phit1;
+
+    while(psit >= 0){
+        phit1 = phit - (psit * dt);
+        psit1 = psit - (w * sin(phit) * dt); /*
+        printf("Angle: %lf\n", phit1);
+        printf("Speed: %lf\n", psit1); */
+
+        n++;
+        psit = psit1;
+        phit = phit1;
+    }
+
+    n += (n + 1);
+    return(n * dt);
+}
+
+double speed_to_no_speed(double phi0, double psi0, double dt, double l, double g){
+    double energy, height, angle, speed;
+
+    speed = psi0 * l;
+
+    height = l * (1 - cos(phi0));
+    //e = (v^2)/2 + gh
+    energy = speed * speed / 2;
+    energy += g * height;
+
+    //e = gh = gl(1 - cos(phi))
+    height = energy / g;
+    height /= l;
+
+    if(fabs(1 - height) > 1){
+        printf("\x1b[31mToo much energy!\x1b[0m\n");
+        return 0;
+    }
+    angle = acos(1 - height);
+    printf("%lf\n", angle);
+
+    return(oscillator_no_speed(angle, dt, l, g));
+}
+
+int main(){
+    double l = -1;
+    double g = 9.80665;
+
+    printf("Enter the length of your pendulum: ");
+    if(1 != scanf("%lf", &l)){
+        perror("Scanf failed");
+        return -1;
+    }
+
+    if(0 >= l){
+        printf("\x1b[31mError: length has to be more than 0\n\x1b[0m");
+        return 0;
+    }
+
+    double phi0 = 0;
+    printf("Enter the starting angle of your pendulum: ");
+    if(1 != scanf("%lf", &phi0)){
+        perror("Scanf failed");
+        return -1;
+    }
+    if(fabs(phi0) - M_PI > -1e-14){
+        printf("\x1b[31mError: too big starting angle\n\x1b[0m");
+        return 0;
+    }
+
+    double psi0 = 0;
+    printf("Enter the starting speed of your pendulum: ");
+    if(1 != scanf("%lf", &psi0)){
+        perror("Scanf failed");
+        return -1;
+    }
+
+    if(1e-14 >= fabs(psi0) && 1e-14 >= fabs(phi0)){
+        printf("\x1b[31mError: no movement\n\x1b[0m");
+        return 0;
+    }
+
+    double dt = 0;
+    printf("Enter a time step for calculating (in seconds): ");
+    if(1 != scanf("%lf", &dt)){
+        perror("Scanf failed");
+        return -1;
+    }
+
+    if(1e-14 >= dt){
+        printf("\x1b[31mError: step has to be more than 0\n\x1b[0m");
+        return 0;
+    }
+
+    double result;
+    if(1e-14 >= fabs(psi0)) result = oscillator_no_speed(phi0, dt, l, g);
+    else result = speed_to_no_speed(phi0, psi0, dt, l, g);
+
+    if(0 == result) return 0;
+    printf("Period: %lf\n", result);
+    double T2 = 2 * M_PI * sqrt(l/g);
+    printf("Theoretical period: %lf\n", T2);
+    printf("Difference: %lf\n", fabs(result - T2));
+    return 1;
+}
