68cb1725a24c02d753e96051f0f7634e0e67880a
EvilArtillery <melkijdruk@gmail.com>
2024-10-09 20:32:05 +0300

    Integral work 3

--- Integral/lib.c
+++ Integral/lib.c
@@ -7,7 +7,9 @@
 double integral(double a, double b, double* f(double)){
   double result;
   result = f(a) + f(b);
-  
+  result *= (b - a);
+  result /= 2.;
+  return result;
 }
 
 double divide(double a, double b, double* f(double)){
