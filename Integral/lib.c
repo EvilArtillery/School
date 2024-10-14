9e17acdec56e4ca650840a79687d6c640c476fd3
EvilArtillery <melkijdruk@gmail.com>
2024-10-14 17:40:52 +0300

    Integral work 4
    lib.c probably complete

--- Integral/lib.c
+++ Integral/lib.c
@@ -4,7 +4,7 @@
 
 #include "lib.h"
 
-double integral(double a, double b, double* f(double)){
+double integral(double a, double b, double f(double)){
   double result;
   result = f(a) + f(b);
   result *= (b - a);
@@ -12,6 +12,19 @@ double integral(double a, double b, double* f(double)){
   return result;
 }
 
-double divide(double a, double b, double* f(double)){
-  return 0;
+double divide(double a, double b, double f(double), double delta){
+  double sum1, sumhalfs;
+  double middle = (a + b) / 2.;
+
+  sum1 = integral(a, b, f);
+  if(delta > (middle - a)) return sum1;
+
+  sumhalfs = integral(a, middle, f) + integral(middle, b, f);
+
+  if(delta > fabs(sum1 - sumhalfs)){
+    double result = divide(a, middle, f, delta) + divide(middle, b, f, delta);
+    return result;
+  }
+
+  return sum1;
 }
