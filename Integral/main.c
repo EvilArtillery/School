383da6bd0d181cf06a0882bca51726314b5e1d9c
EvilArtillery <melkijdruk@gmail.com>
2024-11-16 17:05:43 +0300

    Integral optimization 2

--- Integral/main.c
+++ Integral/main.c
@@ -47,19 +47,32 @@ int main(){
 
   double length = (b - a) / 100.;
   double x1 = a;
+  double I1, I2;
+  double fx1, fx2, fx15;
 
   while(x1 < b){
-    if(divide(x1, x1 + length, function, delta)){
-      length /= 2.;
+    fx1 = function(x1);
+    fx2 = function(x1 + length);
+    fx15 = function(x1 + (length * 0.5));
+
+    I1 = integral(x1, x1 + length, function, fx1, fx2);
+    I2 = integral(x1, x1 + (length * 0.5), function, fx1, fx15) + integral(x1 + (length * 0.5), x1 + length, function, fx15, fx2);
+
+    if(1e-12 < fabs(I1 - I2)){
+      if(delta < (length * 0.5)){
+        length *= 0.5;
+        continue;
+      }
     }
-    else if(unite(x1, x1 + length, x1 + (2 * length), function)){
+    else if(1e-13 > fabs(I1 - I2)){
       length *= 2;
+      continue;
     }
-    else{
-      result += integral(x1, x1 + length, function, NAN, NAN);
-      x1 += length;
-//      printf("Step, l=%lf\n", length);
-    }
+
+    result += integral(x1, x1 + length, function, NAN, NAN);
+    x1 += length;
+//    printf("Step, l=%lf\n", length);
+    
   }
 
 
