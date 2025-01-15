8951495e69898b5d7d728fc27e8315bcda352e0e
EvilArtillery <melkijdruk@gmail.com>
2025-01-15 19:29:17 +0300

    approximation work started

---
+++ Approximation/Petrov_DS.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct dot {
+    double x;
+    double y;
+} dot;
+
+double cmod(dot a){
+	double b;
+	b = a.x * a.x + (a.y * a.y);
+	return sqrt(b);
+}
+
+double argc(dot a){
+	double ar = 0;
+	double mod = cmod(a);
+	if(a.y > 1e-14) ar = acos(a.x/mod);
+	else ar = - acos(a.x/mod);
+	return ar;
+}
+
+dot sqrtc(dot a){
+	if(fabs(cmod(a) < 1e-10)) return (dot){0, 0};
+	dot b;
+	double r = cmod(a), fi = argc(a);
+	fi /= 2.;
+	r = sqrt(r);
+	b.x = r * cos(fi);
+	b.y = r * sin(fi);
+	return b;
+}
+
+
+void main(){
+    int n;
+    if(1 != scanf("%i", &n)){
+        printf("\x1b[91mError! Scanf failed!\x1b[0m\n");
+        return;
+    }
+
+    dot* array = malloc(n * sizeof(dot));
+    if(array == NULL){
+        perror("Malloc failed");
+        return;
+    }
+
+    for(int i = 0; i < n; i++){
+        if(2 != scanf("%lf %lf", &(array+i)->x, &(array+i)->y)){
+            printf("\x1b[91mError! Scanf failed!\x1b[0m\n");
+            free(array);
+            return;
+        }
+    }
+
+    dot centroid = {0, 0};
+
+    for(int i = 0; i < n; i++){
+        centroid.x += array[i].x;
+        centroid.y += array[i].y;
+    }
+
+    centroid.x /= n;
+    centroid.y /= n;
+
+    dot S = {0, 0};
+    double xi = 0, yi = 0;
+
+    for(int i = 0; i < n; i++){
+        xi = centroid.x - array[i].x;
+        yi = centroid.y - array[i].y;
+        xi *= xi;
+        yi *= yi;
+        S.x += xi;
+        S.y += yi;
+    }
+
+    dot S1 = sqrtc(S);
+
+    double answera, answerb, answerc;
+
+    answera = -S1.y;
+    answerb = S1.x;
+    answerc = S.x * answera + S.y * answerb;
+    answerc *= -1;
+
+    printf("Answer:\n");
+    printf("\x1b[92m");
+    printf("%lfx + %lfy + %lf = 0", answera, answerb, answerc);
+    printf("\n\x1b[0m");
+
+    free(array);
+    return;
+}
